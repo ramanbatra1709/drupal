89cfc1f593b6eaa31e4e181dd752a5a7a29d65fe
Raman Batra <raman.batra1709@gmail.com>
2018-10-21 16:00:13 +0530

    Use an existing service (logger.factory) to log the message

--- modules/aargh/src/Controller/AarghController.php
+++ modules/aargh/src/Controller/AarghController.php
@@ -10,24 +10,30 @@ namespace Drupal\aargh\Controller;
 
 use Drupal\aargh\Services\AarghGenerator;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class AarghController extends ControllerBase {
 
     private $aarghGenerator;
+    private $loggerFactoryService;
 
-    public function __construct(AarghGenerator $aarghGenerator) {
+    public function __construct(AarghGenerator $aarghGenerator, LoggerChannelFactoryInterface $loggerFactoryService) {
         $this->aarghGenerator = $aarghGenerator;
+        $this->loggerFactoryService = $loggerFactoryService;
     }
 
     public function aargh($num) {
         $aargh = $this->aarghGenerator->getAargh($num);
+        $this->loggerFactoryService->get('default')
+            ->debug($aargh);
         return new Response($aargh);
     }
 
     public static function create(ContainerInterface $container) {
         $aarghGenerator = $container->get('aargh.aargh_generator');
-        return new static($aarghGenerator);
+        $loggerFactoryService = $container->get('logger.factory');
+        return new static($aarghGenerator, $loggerFactoryService);
     }
 }

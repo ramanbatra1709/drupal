d35c56b6b0d9d37db28b54f54cd611f05f5a8e75
Raman Batra <raman.batra1709@gmail.com>
2018-10-21 15:47:34 +0530

    Use the service container (apply dependecy injection)

--- modules/aargh/src/Controller/AarghController.php
+++ modules/aargh/src/Controller/AarghController.php
@@ -9,12 +9,25 @@
 namespace Drupal\aargh\Controller;
 
 use Drupal\aargh\Services\AarghGenerator;
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
-class AarghController {
+class AarghController extends ControllerBase {
+
+    private $aarghGenerator;
+
+    public function __construct(AarghGenerator $aarghGenerator) {
+        $this->aarghGenerator = $aarghGenerator;
+    }
+
     public function aargh($num) {
-        $aarghGenerator = new AarghGenerator();
-        $aargh = $aarghGenerator->getAargh($num);
+        $aargh = $this->aarghGenerator->getAargh($num);
         return new Response($aargh);
+    }
+
+    public static function create(ContainerInterface $container) {
+        $aarghGenerator = $container->get('aargh.aargh_generator');
+        return new static($aarghGenerator);
     }
 }
